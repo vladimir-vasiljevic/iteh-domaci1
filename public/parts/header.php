b25e25795ffc80522993ce3188d9084a3f4247c5
Vladimir Vasiljevic <vv20130462@student.fon.bg.ac.rs>
2023-02-11 00:06:36 +0100

    Ažuriranje jquery-ja.

--- public/parts/header.php
+++ public/parts/header.php
@@ -9,6 +9,6 @@
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
 
-        <script type="text/javascript" src="js/jquery/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery/jquery-3.6.3.min.js"></script>
         <script type="text/javascript" src="js/bootstrap/bootstrap.bundle.min.js"></script>
     </head>
